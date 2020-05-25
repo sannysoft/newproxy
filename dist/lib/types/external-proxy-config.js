35a34a6f26af11ea8f6166ca994871465981a483
sannysoft <sannysoft@gmail.com>
2020-05-25 14:24:27 +0300

    bug fix

--- dist/lib/types/external-proxy-config.js
+++ dist/lib/types/external-proxy-config.js
@@ -7,9 +7,16 @@ var ExternalProxyHelper = /** @class */ (function () {
         this.config = config;
     }
     ExternalProxyHelper.prototype.getUrlObject = function () {
-        if (typeof this.config === 'string')
-            return url.parse(this.config);
-        return url.parse(this.config.url);
+        var proxy;
+        if (typeof this.config === 'string') {
+            proxy = this.config;
+        }
+        else {
+            proxy = this.config.url;
+        }
+        if (!proxy.startsWith('http:') && !proxy.startsWith('https:'))
+            proxy = "http://" + proxy;
+        return url.parse(proxy);
     };
     ExternalProxyHelper.prototype.getProtocol = function () {
         return this.getUrlObject().protocol || '';
